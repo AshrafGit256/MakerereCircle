4db43b2437f33cb01a4ac3f1c0cbf677c459d7bd
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2025-08-17 00:51:31 +0300

    Created group routes

---
+++ app/Livewire/Groups/Index.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Livewire\Groups;
+
+use App\Models\Group;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public $search = '';
+    
+    protected $queryString = ['search'];
+    
+    public function render()
+    {
+        $groups = Group::with('members')
+            ->when($this->search, function ($query) {
+                $query->where('name', 'like', '%' . $this->search . '%')
+                      ->orWhere('description', 'like', '%' . $this->search . '%');
+            })
+            ->orderBy('name')
+            ->paginate(12);
+        
+        return view('livewire.groups.index', [
+            'groups' => $groups
+        ]);
+    }
+    
+    public function createGroup()
+    {
+        abort_unless(auth()->check(), 401);
+        
+        $this->validate([
+            'newGroupName' => 'required|string|max:255',
+            'newGroupDescription' => 'nullable|string|max:1000',
+        ]);
+        
+        $group = Group::create([
+            'name' => $this->newGroupName,
+            'description' => $this->newGroupDescription,
+            'slug' => \Str::slug($this->newGroupName) . '-' . time(),
+            'user_id' => auth()->id(),
+        ]);
+        
+        // Add creator as member
+        $group->members()->attach(auth()->id());
+        
+        $this->reset('newGroupName', 'newGroupDescription');
+        $this->dispatch('group-created');
+    }
+}
